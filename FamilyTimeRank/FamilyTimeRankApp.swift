4d507180f5b2bf7dd25859647d3b57cd574eaef8
gen <genmanabu@gmail.com>
2026-01-04 08:52:26 +0900

    Initial Commit

---
+++ FamilyTimeRank/FamilyTimeRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyTimeRankApp.swift
+//  FamilyTimeRank
+//
+//  Created by 厳恒 on 2026/01/04.
+//
+
+import SwiftUI
+
+@main
+struct FamilyTimeRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
