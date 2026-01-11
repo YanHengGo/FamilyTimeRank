62094eb47f74e4b388241417b28517cad47baf25
gen <genmanabu@gmail.com>
2026-01-11 18:37:48 +0900

    firebase連携delegate追加

---
+++ FamilyTimeRank/Infrastructure/Firebase/FirebaseAppDelegate.swift
@@ -0,0 +1,13 @@
+import UIKit
+import FirebaseCore
+
+final class FirebaseAppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
